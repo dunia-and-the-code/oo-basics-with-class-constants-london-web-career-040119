8a5dd0f63171a361c99b487e9bc96d36cd7f0bd5
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-04-04 16:56:51 +0100

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,33 @@
+require "pry"
+
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+
   def initialize(title)
     @title = title
+    @author =  author
+    @page_count = page_count
+    @genre = genre
+    # GENRES << genre
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def self.all
+    GENRES
+  end
+  #
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
 end
+
+# binding.pry
+# "<3"
